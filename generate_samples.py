c82a9723b5a0e1196c876863b04a803646e4e8fe
Kiran Rao <kiran_rao@outlook.com>
2017-02-25 17:46:59 -0500

    Something something display clusters

--- generate_samples.py
+++ generate_samples.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 from functions import create_samples
+from functions import plot_clusters
 
 n_features = 2
 n_clusters = 3
@@ -13,7 +14,9 @@ np.random.seed(seed)
 
 centroids, samples = create_samples(n_clusters, n_samples_per_cluster, n_features, embiggen_factor, seed)
 
-model = tf.initialize_all_variables()
+model = tf.global_variables_initializer()
 with tf.Session() as session:
-    sample_values = session.run(samples)
-    centroid_values = session.run(centroids)
+	sample_values = session.run(samples)
+	centroid_values = session.run(centroids)
+	print sample_values, centroid_values
+	plot_clusters(sample_values, centroid_values, n_samples_per_cluster)
