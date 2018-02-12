f9eb4c23345624c36b3cb4b683a323893febe2aa
Kiran Rao <kiran_rao@outlook.com>
2018-02-11 21:29:27 -0500

    Expirementing with optimizers

--- keras/Readme.md
+++ keras/Readme.md
@@ -1,3 +1,23 @@
 # Some playing around with Keras
 
 https://blog.keras.io/keras-as-a-simplified-interface-to-tensorflow-tutorial.html
+
+
+Notes for on optimization performance:
+
+All training done with 10000 mini batches of 50
+
+2x Dense Networks (128): 0.95
+Increase layer density (256): no diff
+Add dropout (0.5): 0.97
+Add additional layer: no diff
+Change training rate to 0.1: Slightly slower, but more stable error: ~0.975
+Use Momentum Optimizer (0.1, 0.5): Much faster training, 0.98
+
+Use Momentum(0.1, 0.5) for 5000, Momentum(0.05, 0.5): 0.97~
+
+Use Momentum(0.1, 0.5) for 5000, GradientDescent(0.05) for 5000
+- Much more stable, but still only getting 0.98
+
+Moved to 256, got 0.983
+3 layers of 128 got 0.982
--- keras/test1.py
+++ keras/test1.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 from keras import backend as K
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.objectives import categorical_crossentropy
-from keras.metrics import categorical_accuracy as accuracy
+from keras.metrics import categorical_accuracy
 
 # TF session
 sess = tf.Session()
@@ -15,9 +15,11 @@ K.set_session(sess)
 img = tf.placeholder(tf.float32, shape=(None, 784))
 
 # Network
-x_1 = Dense(128, activation='relu')(img)
-x_2 = Dense(128, activation='relu')(x_1)
-preds = Dense(10, activation='softmax')(x_2)
+x = Dense(256, activation='relu')(img)
+x = Dropout(0.5)(x)
+x = Dense(256, activation='relu')(x)
+x = Dropout(0.5)(x)
+preds = Dense(10, activation='softmax')(x)
 
 # Label placeholder
 labels = tf.placeholder(tf.float32, shape=(None, 10))
@@ -27,10 +29,13 @@ loss = tf.reduce_mean(categorical_crossentropy(labels, preds))
 
 # training data
 mnist_data = input_data.read_data_sets('MNIST_data', one_hot=True)
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+# optimizer
+train_step = tf.train.MomentumOptimizer(0.1, 0.5).minimize(loss)
+train_step_late = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
 
 # training accuracy
-acc_value = accuracy(labels, preds)
+acc_value = categorical_accuracy(labels, preds)
 
 
 # init all
@@ -40,13 +45,21 @@ sess.run(init_op)
 
 # train
 with sess.as_default():
-    res = acc_value.eval(feed_dict={img: mnist_data.test.images, labels: mnist_data.test.labels})
-    print('Random Accuracy' , np.mean(res))
+    accuracy = acc_value.eval(feed_dict={img: mnist_data.test.images, labels: mnist_data.test.labels})
+    print('Random Accuracy' , np.mean(accuracy))
 
-    for j in range(20):
-        for i in range(50):
+    for j in range(5000//100):
+        for i in range(100):
             batch = mnist_data.train.next_batch(50)
             train_step.run(feed_dict={img: batch[0], labels: batch[1]})
 
-        res = acc_value.eval(feed_dict={img: mnist_data.test.images, labels: mnist_data.test.labels})
-        print('Accuracy', (j + 1) * 50, np.mean(res))
+        accuracy = acc_value.eval(feed_dict={img: mnist_data.test.images, labels: mnist_data.test.labels})
+        print('Accuracy', (j + 1) * 100, np.mean(accuracy))
+
+    for j in range(5000//100):
+        for i in range(100):
+            batch = mnist_data.train.next_batch(50)
+            train_step_late.run(feed_dict={img: batch[0], labels: batch[1]})
+
+        accuracy = acc_value.eval(feed_dict={img: mnist_data.test.images, labels: mnist_data.test.labels})
+        print('Accuracy, Late', (j + 1) * 100, np.mean(accuracy))
